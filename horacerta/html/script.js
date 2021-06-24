7fc0442c0b7dfca9229c747ee68f464d1dff9f97
hiure <hiure@riseup.net>
2021-06-24 13:26:44 -0300

    corrigindo tudo

--- horacerta/html/script.js
+++ horacerta/html/script.js
@@ -1,1 +1,26 @@
-var a = 2
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+   // msg.innerHTML = `Agora são ${hora}:${minuto}`
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        msg.innerHTML += `Bom dia! Agora são ${hora}:${minuto}`
+        img.src = 'image/manha.png'
+        document.body.style.background = '#fe9949'
+    } else if (hora >= 12 && hora < 18){
+        //boa tarde
+        msg.innerHTML += `...Boa Tarde! Agora são ${hora}:${minuto}`
+        img.src = 'image/tarde.png'
+        document.body.style.background = '#f4b357'
+    } else {
+        msg.innerHTML += `...Boa Noite! Agora são ${hora}:${minuto}`
+        img.src = 'image/noite.png'
+        document.body.style.background = '#003261'
+    }
+    
+    
+}
+
