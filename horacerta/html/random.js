0e5cc54f6a67aff0959c40525fc9c340eee59c4f
hiure <hiure@debian-BULLSEYE-live-builder-AMD64>
2022-09-07 16:32:37 -0300

    adiciona redirect random links

---
+++ horacerta/html/random.js
@@ -0,0 +1,75 @@
+function random_a(){
+    var myrandom=Math.round(Math.random()*4)
+    var link1="https://www.youtube.com/watch?v=QzmyCxFUADk"
+    var link2="https://www.youtube.com/watch?v=kZhPvruoeFw"
+    var link3="https://open.spotify.com/track/5LgtJshfcCKoi4LQ40ym4d?si=2f1105c13da648de"
+    var link4="https://www.youtube.com/watch?v=VgeM7Qnpvxs"
+    var link5="https://fotografia.folha.uol.com.br/galerias/1711039918826770-coberta-de-ouro-elle-de-bernardini-questiona-aceitacao-do-corpo-trans-pela-sociedade-veja-fotografias-da-performance"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+}
+
+function random_b(){
+    var myrandom=Math.round(Math.random()*4)
+    var link1="https://www.youtube.com/watch?v=UMRhqOMEZNE "
+    var link2="https://open.spotify.com/track/5QVKz7sKtxhr6DxyXMtBbA?si=62a48f8129184e8c "
+    var link3="https://www.instagram.com/p/BzD3uWdAiwL/?utm_source=ig_embed&ig_rid=3c050630-a3d1-4ac4-a468-3d7d3fcf17df"
+    var link4="/"
+    var link5="/"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+}
+
+function random_c(){
+    var myrandom=Math.round(Math.random()*4)
+    var link1="https://www.instagram.com/p/B6EbD8kAKbd/"
+    var link2="https://open.spotify.com/track/1wKSqAWgu2Cu8AO0c04A24?si=74831038300149b6"
+    var link3="https://www.uol.com.br/universa/noticias/redacao/2017/09/17/trans-mostra-corpo-definido-e-os-seios-cobertos-para-celebrar-transicao.htm"
+    var link4="/"
+    var link5="/"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+}
+
+function random_d(){
+    var myrandom=Math.round(Math.random()*4)
+    var link1="https://www.youtube.com/watch?v=3EehckxB2qU "
+    var link2="https://open.spotify.com/track/5uDxxT5hBxtOj7qTR0WCoB?si=b108b98731914334 "
+    var link3="/"
+    var link4="/"
+    var link5="/"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+}
