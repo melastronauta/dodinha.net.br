614583ff06714ec5b7c38c0933fed279d3d39fc1
hiure <hiure@debian-BULLSEYE-live-builder-AMD64>
2022-09-14 12:39:04 -0300

    add random links

--- horacerta/html/random.js
+++ horacerta/html/random.js
@@ -22,8 +22,8 @@ function random_b(){
     var link1="https://www.youtube.com/watch?v=UMRhqOMEZNE "
     var link2="https://open.spotify.com/track/5QVKz7sKtxhr6DxyXMtBbA?si=62a48f8129184e8c "
     var link3="https://www.instagram.com/p/BzD3uWdAiwL/?utm_source=ig_embed&ig_rid=3c050630-a3d1-4ac4-a468-3d7d3fcf17df"
-    var link4="/"
-    var link5="/"
+    var link4="https://recantodopoeta.com/breve-memoria/"
+    var link5="https://youtu.be/t-J29RasN7w"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
@@ -41,8 +41,8 @@ function random_c(){
     var link1="https://www.instagram.com/p/B6EbD8kAKbd/"
     var link2="https://open.spotify.com/track/1wKSqAWgu2Cu8AO0c04A24?si=74831038300149b6"
     var link3="https://www.uol.com.br/universa/noticias/redacao/2017/09/17/trans-mostra-corpo-definido-e-os-seios-cobertos-para-celebrar-transicao.htm"
-    var link4="/"
-    var link5="/"
+    var link4="https://www.instagram.com/p/BzD3uWdAiwL/?utm_source=ig_embed&ig_rid=3c050630-a3d1-4ac4-a468-3d7d3fcf17df"
+    var link5="https://www.premiopipa.com/fefa-lins/"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
@@ -59,9 +59,9 @@ function random_d(){
     var myrandom=Math.round(Math.random()*4)
     var link1="https://www.youtube.com/watch?v=3EehckxB2qU "
     var link2="https://open.spotify.com/track/5uDxxT5hBxtOj7qTR0WCoB?si=b108b98731914334 "
-    var link3="/"
-    var link4="/"
-    var link5="/"
+    var link3="https://youtu.be/VzYbZr4K9Lg?t=74"
+    var link4="https://www.instagram.com/p/CgP4x9bPZUC/?utm_source=ig_web_copy_link"
+    var link5="https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/cindy-sherman/81748-008797024368/cindy-sherman-doctor-and-nurse-1-800x800.jpg"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
