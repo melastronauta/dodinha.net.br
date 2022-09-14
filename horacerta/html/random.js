452c48418fe435101a11346f058143befda550e0
melastronauta <113062473+melastronauta@users.noreply.github.com>
2022-09-13 22:12:33 -0300

    Update random.js

--- horacerta/html/random.js
+++ horacerta/html/random.js
@@ -1,10 +1,10 @@
 function random_a(){
     var myrandom=Math.round(Math.random()*4)
-    var link1="https://www.youtube.com/watch?v=QzmyCxFUADk"
-    var link2="https://www.youtube.com/watch?v=kZhPvruoeFw"
-    var link3="https://open.spotify.com/track/5LgtJshfcCKoi4LQ40ym4d?si=2f1105c13da648de"
-    var link4="https://www.youtube.com/watch?v=VgeM7Qnpvxs"
-    var link5="https://fotografia.folha.uol.com.br/galerias/1711039918826770-coberta-de-ouro-elle-de-bernardini-questiona-aceitacao-do-corpo-trans-pela-sociedade-veja-fotografias-da-performance"
+    var link1="https://open.spotify.com/thttps://open.spotify.com/track/5LgtJshfcCKoi4LQ40ym4d?si=2f1105c13da648derack/5LgtJshfcCKoi4LQ40ym4d?si=2f1105c13da648de"
+    var link2="https://youtu.be/VgeM7Qnpvxs?t=7"
+    var link3="https://youtu.be/Q8LyEErq2ws?t=5"
+    var link4="https://www.youtube.com/watch?v=Bxbuqll1F0w&list=RDBxbuqll1F0w&start_radio=1"
+    var link5="https://dodileal.wixsite.com/arte/tetagrafias"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
@@ -19,11 +19,11 @@ function random_a(){
 
 function random_b(){
     var myrandom=Math.round(Math.random()*4)
-    var link1="https://www.youtube.com/watch?v=UMRhqOMEZNE "
-    var link2="https://open.spotify.com/track/5QVKz7sKtxhr6DxyXMtBbA?si=62a48f8129184e8c "
-    var link3="https://www.instagram.com/p/BzD3uWdAiwL/?utm_source=ig_embed&ig_rid=3c050630-a3d1-4ac4-a468-3d7d3fcf17df"
-    var link4="/"
-    var link5="/"
+    var link1="https://www.youtube.com/watch?v=QzmyCxFUADk "
+    var link2="https://www.youtube.com/watch?v=UMRhqOMEZNE "
+    var link3="https://open.spotify.com/track/5QVKz7sKtxhr6DxyXMtBbA?si=62a48f8129184e8c"
+    var link4="https://recantodopoeta.com/breve-memoria/"
+    var link5="https://youtu.be/t-J29RasN7w"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
@@ -38,11 +38,11 @@ function random_b(){
 
 function random_c(){
     var myrandom=Math.round(Math.random()*4)
-    var link1="https://www.instagram.com/p/B6EbD8kAKbd/"
+    var link1="https://www.instagram.com/p/B6EbD8kAKbd/ "
     var link2="https://open.spotify.com/track/1wKSqAWgu2Cu8AO0c04A24?si=74831038300149b6"
-    var link3="https://www.uol.com.br/universa/noticias/redacao/2017/09/17/trans-mostra-corpo-definido-e-os-seios-cobertos-para-celebrar-transicao.htm"
-    var link4="/"
-    var link5="/"
+    var link3="https://www.uol.com.br/universa/noticias/redacao/2017/09/17/trans-mostra-corpo-definido-e-os-seios-cobertos-para-celebrar-transicao.htm "
+    var link4="https://www.instagram.com/p/BzD3uWdAiwL/?utm_source=ig_embed&ig_rid=3c050630-a3d1-4ac4-a468-3d7d3fcf17df"
+    var link5="https://www.premiopipa.com/fefa-lins/"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
@@ -57,11 +57,11 @@ function random_c(){
 
 function random_d(){
     var myrandom=Math.round(Math.random()*4)
-    var link1="https://www.youtube.com/watch?v=3EehckxB2qU "
-    var link2="https://open.spotify.com/track/5uDxxT5hBxtOj7qTR0WCoB?si=b108b98731914334 "
-    var link3="/"
-    var link4="/"
-    var link5="/"
+    var link1="https://mutha.com.br/"
+    var link2="https://open.spotify.com/track/5uDxxT5hBxtOj7qTR0WCoB?si=b108b98731914334"
+    var link3="https://youtu.be/VzYbZr4K9Lg?t=74"
+    var link4="https://www.instagram.com/p/CgP4x9bPZUC/?utm_source=ig_web_copy_link "
+    var link5="https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/cindy-sherman/81748-008797024368/cindy-sherman-doctor-and-nurse-1-800x800.jpg"
     if (myrandom==0)
         window.location=link1
     else if (myrandom==1)
